8e4d52169fd7a94ba00297fdee98f5872dccddda
Nadja <92398598+NadjaGrothe@users.noreply.github.com>
2022-03-28 11:52:32 +0100

    fixed populatetable script

--- db/script/populateTable.js
+++ db/script/populateTable.js
@@ -6,7 +6,7 @@ async function populateTable() {
       const rec_exp_name = data[i].rec_exp_name;
       const rec_exp_amount = data[i].rec_exp_amount;
       const res = await query(
-         `INSERT INTO personnel(rec_exp_name,rec_exp_amount)VALUES($1, $2) RETURNING *`,
+         `INSERT INTO recurring(rec_exp_name,rec_exp_amount)VALUES($1, $2) RETURNING *`,
          [rec_exp_name, rec_exp_amount]
       );
 
